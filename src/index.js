ea475c6291c66092105dfd3fa78f26fe71451f49
Wangdi Tenzing <wangdi457@gmail.com>
2020-02-05 13:22:07 -0500

    Created the store and set up intial state for the reducer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+// import {}
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+
+const store=creatStore(reducer)
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
